3478e3ca6b633380f92909f09ae0ad8001a207a9
Irina <irashilowa1997@mail.ru>
2024-09-29 22:42:10 +0300

    Первое задание проверка

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,29 @@
 "use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+function solveEquation(a, b, c) {
+		let arr = [];
+		return arr;
+	}
+
+	let discriminant = Math.pow(b, 2) - 4 * a * c;
+
+	
+	if (discriminant = 0) {
+		let oneRoot = -b / (2 * a);
+		return solveEquation;
+	} else if (discriminant > 0) {
+		let root1 = (-b + Math.sqrt(d)) / (2 * a);
+		let root2 = (-b - Math.sqrt(d)) / (2 * a);
+		return solveEquation;
+	} else if (discriminant < 0) {
+		return [];
+	} 
+
+
+
+
+
+
+ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
 }
