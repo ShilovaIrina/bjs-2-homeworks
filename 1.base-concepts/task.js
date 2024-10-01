d2e0b62489b05ab51f6e5d9306efa169da650d34
Irina <irashilowa1997@mail.ru>
2024-10-01 17:22:08 +0300

    Первое и задание полностью выполнены

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,29 +1,31 @@
 "use strict"
 
 function solveEquation(a, b, c) {
-		let arr = [];
-		return arr;
-	}
-
+	let arr = [];
 	let discriminant = Math.pow(b, 2) - 4 * a * c;
 
-	
-	if (discriminant = 0) {
-		let oneRoot = -b / (2 * a);
-		return solveEquation;
-	} else if (discriminant > 0) {
-		let root1 = (-b + Math.sqrt(d)) / (2 * a);
-		let root2 = (-b - Math.sqrt(d)) / (2 * a);
-		return solveEquation;
-	} else if (discriminant < 0) {
-		return [];
-	} 
+	if (discriminant < 0) {
+		return arr;
+	} else if (discriminant === 0) {
+		let root = -b / (2 * a);
+		arr.push(root);
+		return arr;
+	} else {
+		let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+		let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+		arr.push(root1, root2);
+		return arr;
+	}
+}
 
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+	let perset1 = percent / 100;
+	let p = perset1 / 12;
 
-
-
-
-
- function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let credit = amount - contribution;
+	let monthlyPayment = credit * (p + (p / (((1 + p) ** countMonths) - 1)));
+	let result = monthlyPayment * countMonths;
+	let resultCredit = (result).toFixed(2);
+	let x = parseFloat(resultCredit);
+	return x;
 }
