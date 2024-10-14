0e5a3ca54f1e88873fb9da5e671d1fc8e02e10ca
Irina <irashilowa1997@mail.ru>
2024-10-14 14:11:04 +0300

    Задание готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,37 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
+
+	this.setSubject = function(subjectName) {
+		this.subject = subjectName;
+	}
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.addMarks = function(...marksToAdd) {
+	if (!this.marks) {
+		console.log("Ошибка: Студент отчислен");
+		return;
+	}
+
+	if (!!marksToAdd.length) {
+		this.marks.push(...marksToAdd);
+	}
+	return;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.getAverage = function() {
+	if (!this.marks || !this.marks.length) {
+		return 0;
+	}
+
+	const summ = this.marks.reduce((acc, curr) => acc + curr, 0);
+	return summ / this.marks.length;
 }
 
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
 }
